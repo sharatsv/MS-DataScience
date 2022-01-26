5e4a86c9d2b65e9e9492800a5476ba27747701f0
sharatsv <sharatsv@gmail.com>
2022-01-26 02:51:38 -0800

    Delete IST-652 directory

--- IST-652/Sharat_Sripada_First_program.py
+++
@@ -1,4 +0,0 @@
-hours=input('Hours worked: ')
-rate=input('Rate of pay: ')
-pay = int(hours) * int(rate)
-print('Pay: {0}'.format(pay))
--- IST-652/Sharat_Sripada_Functions_4.2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-def computepay(hours, rate):
-    print('Pay: {:.2f}'.format(int(hours) * int(rate)))
-
-in_hours = input('Enter hours:')
-in_rate = input('Enter rate:')
-
-computepay(in_hours, in_rate)
